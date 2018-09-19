1d87454907c305759fdde9ed6e06637288553944
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-09-19 10:40:13 +0200

    Complete homework 7.

--- search1d.py
+++ search1d.py
@@ -4,6 +4,35 @@ import sympy as sy
 
 from IPython.display import HTML, display
 
+def plot_1d_function(x, y, xlimit=(-10, 10), ylimit=None, show_spines=True, figsize=(10, 6)):
+    if ylimit is None:
+        y_high = np.max(y)
+        y_low = np.min(y)
+        space = (y_high - y_low) / 20
+        ylimit = (y_low - space, y_high + space)
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(x, y)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$f(x)$')
+    ax.set_xlim(*xlimit)
+    ax.set_ylim(*ylimit)
+    if show_spines:
+        x_min = min(xlimit[0], xlimit[1])
+        if xlimit[0] < 0 and xlimit[1] > 0:
+            x_min = 0.0
+        y_min = min(ylimit[0], ylimit[1])
+        if ylimit[0] < 0 and ylimit[1] > 0:
+            y_min = 0.0
+        ax.spines['left'].set_position(('data', x_min))
+        ax.spines['bottom'].set_position(('data', y_min))
+        ax.spines['left'].set_smart_bounds(True)
+        ax.spines['bottom'].set_smart_bounds(True)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+
+
 class OneDimensionalIntervalBasedSearch:
     def __init__(self, f, x, initial_range, uncertainty, epsilon=0.0):
         self._f = sy.Matrix([f])
@@ -14,22 +43,10 @@ class OneDimensionalIntervalBasedSearch:
         self._uncertainty = uncertainty
         self._epsilon = epsilon
 
-    def plot(self, ylimit=None):
+    def plot(self, ylimit=None, show_spines=True, figsize=(10, 6)):
         x = np.linspace(self._x_start, self._x_end, 50)
         y = self._f_lambda(x)
-
-        if ylimit is None:
-            y_high = np.max(y)
-            y_low = np.min(y)
-            space = (y_high - y_low) / 20
-            ylimit = (y_low - space, y_high + space)
-
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(x, y)
-        ax.set_xlabel('$x$')
-        ax.set_ylabel('$f(x)$')
-        ax.set_xlim(self._x_start, self._x_end)
-        ax.set_ylim(*ylimit)
+        plot_1d_function(x, y, xlimit=(self._x_start, self._x_end), ylimit=ylimit, show_spines=show_spines, figsize=figsize)
 
     def run(self, num_iterations):
         html_out = (
@@ -159,7 +176,7 @@ class FibonacciSearch(OneDimensionalIntervalBasedSearch):
         return float(result)
 
 
-class NewtonSearch:
+class OneDimensionalIterativeSearch:
     def __init__(self, f, x):
         self._f = sy.Matrix([f])
         self._x = (x,)
@@ -168,72 +185,145 @@ class NewtonSearch:
         self._jacobian_lambda = sy.lambdify(self._x, self._jacobian)
         self._hessian = self._jacobian.jacobian(self._x)
         self._hessian_lambda = sy.lambdify(self._x, self._hessian)
+        
+    def func_at(self, point):
+        """
+        Evaluates the function f at the given point.
+        """
+        return self._f_lambda(point)
+    
+    def derivative_at(self, point):
+        """
+        Evaluates the first derivative f' at the given point.
+        """
+        return np.asscalar(self._jacobian_lambda(point))
+    
+    def second_derivative_at(self, point):
+        """
+        Evaluates the second derivative f'' at the given point.
+        """
+        return np.asscalar(self._hessian_lambda(point))
 
-    def evaluate(self, x):
-        return self._f_lambda(x)
-
-    def run(self, starting_point, epsilon, max_iterations=21):
-        x_k = starting_point
-        stop = False
-        for k in range(1, max_iterations):
-            jacob_at_x_k = np.asscalar(self._jacobian_lambda(x_k))
-            hess_at_x_k = np.asscalar(self._hessian_lambda(x_k))
-            x_kp1 = x_k - (jacob_at_x_k / hess_at_x_k)
-            if np.abs(x_kp1 - x_k) < epsilon:
-                stop = True
-            x_k = x_kp1
-            print('Iteration {0:2}: x(k)={1}'.format(k, x_k))
-            if stop:
-                print(' Stopping condition reached!')
-                break
-        if not stop:
-            print(' Stopping condition never reached!')
-        return x_k
-
-    def find_root(self, starting_point, epsilon=0.00001, max_iterations=21):
-        x_k = starting_point
-        stop = False
-        for k in range(1, max_iterations):
-            f_at_x_k = self._f_lambda(x_k)
-            jacob_at_x_k = np.asscalar(self._jacobian_lambda(x_k))
-            x_kp1 = x_k - (f_at_x_k / jacob_at_x_k)
-            if np.abs(x_kp1 - x_k) < epsilon:
-                stop = True
-            x_k = x_kp1
-            print('Iteration {0:2}: x(k)={1}'.format(k, x_k))
-            if stop:
-                print(' Stopping condition reached!')
-                break
-        if not stop:
-            print(' Stopping condition never reached!')
-        return x_k
-
-    def plot(self, xlimit=(-10, 10), ylimit=None, show_spines=False):
+    def plot(self, xlimit=(-10, 10), ylimit=None, show_spines=True, figsize=(10,6)):
         x_start = xlimit[0]
         x_end = xlimit[1]
         x = np.linspace(x_start, x_end, 50)
-        y = self._f_lambda(x)
+        y = self.func_at(x)
+        plot_1d_function(x, y, xlimit=xlimit, ylimit=ylimit, show_spines=show_spines, figsize=figsize)
 
-        if ylimit is None:
-            y_high = np.max(y)
-            y_low = np.min(y)
-            space = (y_high - y_low) / 20
-            ylimit = (y_low - space, y_high + space)
+    def __repr__(self):
+        return self._f[0]
 
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(x, y)
-        ax.set_xlabel('$x$')
-        ax.set_ylabel('$f(x)$')
-        ax.set_xlim(x_start, x_end)
-        ax.set_ylim(*ylimit)
+    def __str__(self):
+        return str(self._f[0])
 
-        if show_spines:
-            ax.spines['left'].set_position('zero')
-            ax.spines['right'].set_color('none')
-            ax.spines['bottom'].set_position('zero')
-            ax.spines['top'].set_color('none')
-            ax.spines['left'].set_smart_bounds(True)
-            ax.spines['bottom'].set_smart_bounds(True)
-            ax.xaxis.set_ticks_position('bottom')
-            ax.yaxis.set_ticks_position('left')
+class NewtonSearch(OneDimensionalIterativeSearch):
+    def __init__(self, f, x):
+        super(NewtonSearch, self).__init__(f, x)
 
+    def _update_for_minimisation(self, x_k):
+        jacob_at_x_k = self.derivative_at(x_k)
+        hess_at_x_k = self.second_derivative_at(x_k)
+        return x_k - (jacob_at_x_k / hess_at_x_k)
+
+    def _update_for_root_finding(self, x_k):
+        f_at_x_k = self.func_at(x_k)
+        jacob_at_x_k = self.derivative_at(x_k)
+        return x_k - (f_at_x_k / jacob_at_x_k)
+    
+    def _run_algorithm(self, starting_point, epsilon, max_iterations, update_rule):
+        x_k = starting_point
+        has_converged = False
+        
+        # Run iterations
+        for k in range(1, max_iterations):
+            # Apply update rule
+            x_kp1 = update_rule(x_k)
+            
+            # Check for convergence
+            if np.abs(x_kp1 - x_k) < epsilon:
+                has_converged = True
+                
+            # Update variables
+            x_k = x_kp1
+            
+            print('Iteration {0:2}: x(k)={1}'.format(k, x_k))
+            if has_converged:
+                print(' Stopping condition reached!')
+                break
+        if not has_converged:
+            print(' Stopping condition never reached!')
+        
+        return x_k
+
+    def find_minimum(self, starting_point, epsilon=0.00001, max_iterations=21):
+        return self._run_algorithm(
+            starting_point, 
+            epsilon, 
+            max_iterations, 
+            self._update_for_minimisation
+        )
+    
+    def find_root(self, starting_point, epsilon=0.00001, max_iterations=21):
+        return self._run_algorithm(
+            starting_point, 
+            epsilon, 
+            max_iterations, 
+            self._update_for_root_finding
+        )
+
+class SecantSearch(OneDimensionalIterativeSearch):
+    def __init__(self, f, x):
+        super(SecantSearch, self).__init__(f, x)
+
+    def _update_for_minimisation(self, x_km1, x_k):
+        jacob_at_x_k   = self.derivative_at(x_k)
+        jacob_at_x_km1 = self.derivative_at(x_km1)
+        return x_k - ((x_k - x_km1) / (jacob_at_x_k - jacob_at_x_km1)) * jacob_at_x_k
+
+    def _update_for_root_finding(self, x_km1, x_k):
+        f_at_x_k   = self.func_at(x_k)
+        f_at_x_km1 = self.func_at(x_km1)
+        return x_k - ((x_k - x_km1) / (f_at_x_k - f_at_x_km1)) * f_at_x_k
+            
+    def _run_algorithm(self, x_minus1, x_0, epsilon, max_iterations, update_rule):
+        x_km1 = x_minus1
+        x_k = x_0
+        has_converged = False
+        for k in range(1, max_iterations):
+            # Apply update rule
+            x_kp1 = update_rule(x_km1, x_k)
+            
+            # Check for convergence
+            if np.abs(x_kp1 - x_k) < np.abs(x_k) * epsilon:
+                has_converged = True
+            
+            # Update variables for next iteration
+            x_km1 = x_k
+            x_k   = x_kp1
+            
+            print('Iteration {0:2}: x({1})={2:.3f} x({3})={4:.3f}'.format(k, k-1, x_km1, k, x_k))
+            if has_converged:
+                print(' Stopping condition reached!')
+                break
+        if not has_converged:
+            print(' Stopping condition never reached!')
+        return x_k
+
+    def find_minimum(self, x_minus1, x_0, epsilon=0.001, max_iterations=21):
+        return self._run_algorithm(
+            x_minus1,
+            x_0,
+            epsilon, 
+            max_iterations, 
+            self._update_for_minimisation
+        )
+    
+    def find_root(self, x_minus1, x_0, epsilon=0.001, max_iterations=21):
+        return self._run_algorithm(
+            x_minus1,
+            x_0,
+            epsilon, 
+            max_iterations, 
+            self._update_for_root_finding
+        )
