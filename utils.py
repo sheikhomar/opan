2d6137b2aba3763bda3001e89abcf50a84f3d745
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-10-16 16:06:24 +0200

    Restudy conjugate methods.

--- utils.py
+++ utils.py
@@ -8,6 +8,11 @@ from IPython.display import HTML, display
 # Show decimal numbers as fractions
 # np.set_printoptions(formatter={'all':lambda x: str(Fraction(x).limit_denominator())})
 
+def load_custom_styles(stylesheet='styles.css'):
+    with open(stylesheet) as f:
+        css = f.read().replace(';', ' !important;')
+    display(HTML('<style type="text/css">%s</style>Stylesheet "%s" loaded.'% (css, stylesheet)))
+
 def to_frac(x):
     return str(Fraction(x).limit_denominator())
 
